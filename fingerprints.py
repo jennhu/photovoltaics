669173b096a9bb86425a2e7b7e88db93231804e1
Jenn <jhu890@gmail.com>
2017-02-06 23:07:15 -0500

    added morgan test, updated fingerprints.py

--- fingerprints.py
+++ fingerprints.py
@@ -4,15 +4,15 @@ import gzip
 # this is for the sketchy molecular fragment stuff
 # import os
 
-from rdkit import Chem
-from rdkit.Chem import rdmolops
 from rdkit import DataStructs
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import rdmolops
 from rdkit.Chem.Fingerprints import FingerprintMols
 from rdkit.Chem import MACCSkeys
-from rdkit.Chem.AtomPairs import Pairs
-from rdkit.Chem import AllChem
-from rdkit.Chem import FragmentCatalog
-from rdkit import RDConfig
+# from rdkit.Chem.AtomPairs import Pairs # atom pairs stuff
+# from rdkit.Chem import FragmentCatalog # molecular fragment stuff
+# from rdkit import RDConfig
 
 # Number of bits for vectors
 NBits = 256
@@ -27,42 +27,40 @@ def export_fps(fps, outfile):
 	print df
 
 # Topological fingerprints
-def top(outfile):
+def top(ms,outfile):
 	top_fps = [rdmolops.RDKFingerprint(m, fpSize=NBits) for m in ms]
 	export_fps(top_fps, outfile)
 
 # Morgan fingerprints
-def morgan(outfile):
-	morgan_fps = [AllChem.GetMorganFingerprintAsBitVect(m,2,nBits=NBits) for m in ms]
+def morgan(ms,outfile):
+	morgan_fps = [AllChem.GetMorganFingerprintAsBitVect(m,2,nBits=NBits) for m in ms_test]
 	export_fps(morgan_fps, outfile)
 
 # MACCS keys
-def maccs(outfile):
+def maccs(ms,outfile):
 	maccs_fps = [MACCSkeys.GenMACCSKeys(m) for m in ms]
 	export_fps(maccs_fps, outfile)
 
 # Atom pairs and topological torsions
-def pair(outfile):
-	pair_fps = [Pairs.GetAtomPairFingerprintAsBitVect(m) for m in ms]
-	export_fps(pair_fps, outfile) 
+#def pair(ms,outfile):
+#	pair_fps = [Pairs.GetAtomPairFingerprintAsBitVect(m) for m in ms]
+#	export_fps(pair_fps, outfile) 
 
-# Read in train and test as Pandas DataFrames
+# FOR TRAINING
+# Read in train as Pandas DataFrame and convert SMILES to molecules
 df_train = pd.read_csv("train.csv.gz", nrows=100000, compression='gzip')
+ms_train = [Chem.MolFromSmiles(s) for s in df_train["smiles"].values]
 
-#print "data done. starting SMILES..."
+# FOR TESTING
+# Read in test as Pandas DataFrame and convert SMILES to molecules
+df_test = pd.read_csv("test.csv.gz", compression='gzip')
+ms_test = [Chem.MolFromSmiles(s) for s in df_test["smiles"].values]
 
-# Convert SMILES to molecules
-ms = [Chem.MolFromSmiles(s) for s in df_train["smiles"].values]
-
-#print "SMILES done. starting top..."
-top('top.csv')
-#print "top done. starting morgan..."
-morgan('morgan.csv')
-#print "morgan done. starting maccs..."
-maccs('maccs.csv')
-#print "maccs done"
+#top(ms,'top.csv')
+morgan(ms_test,'morgan_test.csv')
+#maccs(ms,'maccs.csv')
 # THIS IS VERY SPARSE AND HUMUNGOUS
-# pair('pair.csv')
+# pair(ms,'pair.csv')
 
 # Molecular fragments
 # fName = os.path.join(RDConfig.RDDataDir,'FunctionalGroups.txt')
